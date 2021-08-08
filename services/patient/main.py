1ee3db502c327c6c6394da2610ac4fb4bb1713ea
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2021-08-08 18:25:25 +0530

    dev and prod mode for uvicorn logging added

--- services/patient/main.py
+++ services/patient/main.py
@@ -2,7 +2,12 @@ import os
 import uvicorn
 from api import app
 
-workers = os.cpu_count()
+MODE = os.get_env("MODE", "dev").lower()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="error", workers=workers)
+    if MODE == "dev":
+        uvicorn.run(app, host="0.0.0.0", port=8000, log_level="debug")
+    elif MODE == "prod":
+        uvicorn.run(app, host="0.0.0.0", port=8000, log_level="error")
+    else:
+        print(f"Invalid MODE type! {MODE=}")
