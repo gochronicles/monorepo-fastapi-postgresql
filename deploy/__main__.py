6bd6680e037051bbe66f522b2da083592cbbe25f
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2021-08-10 17:14:48 +0530

    patient ms enabled

--- deploy/__main__.py
+++ deploy/__main__.py
@@ -7,20 +7,23 @@ from components.gcr import create_gcr_image
 # Create resources
 cloud_sql_instance, sql_instance_url = create_database()
 domain_image = create_gcr_image("domain")
-# patient_image = create_gcr_image("patient")
+patient_image = create_gcr_image("patient")
 domain_cloud_run = create_cloud_run_instance(
     cloud_sql_instance, sql_instance_url, domain_image.image_name
 )
-# patient_cloud_run = create_cloud_run_instance(
-#     cloud_sql_instance, sql_instance_url, patient_image
-# )
+patient_cloud_run = create_cloud_run_instance(
+    cloud_sql_instance, sql_instance_url, patient_image
+)
 
 # Export resource info
+# Cloud SQL - POSTGRESQL
 export("cloud_sql_instance_name", cloud_sql_instance.name)
+# Domain Micro Service
 export("domain_full_image", domain_image.image_name)
 export("domain__base_image", domain_image.base_image_name)
-# export("patient_full_image", patient_image.image_name)
-# export("patient__base_image", patient_image.base_image_name)
-
+# Patient Micro Service
+export("patient_full_image", patient_image.image_name)
+export("patient__base_image", patient_image.base_image_name)
+# Cloud Run URL
 export("domain_cloud_run_url", domain_cloud_run.statuses[0].url)
-# export("patient_cloud_run_url", patient_cloud_run.statuses[0].url)
+export("patient_cloud_run_url", patient_cloud_run.statuses[0].url)
